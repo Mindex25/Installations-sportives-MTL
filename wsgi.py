0dc5a299350fd26e12e2fdeca83e0a3447ae2f51
Mindex25 <psygourmet@gmail.com>
2021-12-01 22:27:16 -0500

    x

--- wsgi.py
+++ wsgi.py
@@ -24,6 +24,4 @@ scheduler.start()
 if __name__ == "__main__":
     # Pour la correction
     # maj_donnees()
-    port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host="0.0.0.0", port=port)
     app.run()
